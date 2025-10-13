e2ae1071a43c511fe87554d420350b3b3d04c88d
Bruno Gabriel <brunospagi@gmail.com>
2025-10-13 09:25:28 -0300

    Retirado obrigatoriedade de campos.

--- clientes/forms.py
+++ clientes/forms.py
@@ -1,14 +1,15 @@
 from django import forms # type: ignore
-from .models import Cliente, Historico
+from .models import Cliente
 from django.contrib.auth.models import User # type: ignore
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = '__all__'
-        # O campo 'vendedor' foi removido desta lista para que possamos adicioná-lo dinamicamente
-        exclude = ('data_primeiro_contato', 'data_ultimo_contato', 'data_proximo_contato')
-        
+        fields = [
+            'whatsapp', 'nome_cliente', 'modelo_veiculo', 'valor_estimado_veiculo',
+            'fonte_cliente', 'quantidade_ligacoes', 'tipo_negociacao', 'tipo_contato',
+            'status_negociacao', 'proximo_passo', 'prioridade', 'observacao'
+        ]
         widgets = {
             'observacao': forms.Textarea(attrs={'rows': 3}),
             'whatsapp': forms.TextInput(attrs={'placeholder': '(99) 9 9999-9999'}),
@@ -33,11 +34,3 @@ class ClienteForm(forms.ModelForm):
         # Aplica a classe 'form-control' do Bootstrap a todos os campos
         for field_name, field in self.fields.items():
             field.widget.attrs.update({'class': 'form-control'})
-
-class HistoricoForm(forms.ModelForm):
-    class Meta:
-        model = Historico
-        fields = ['motivacao']
-        widgets = {
-            'motivacao': forms.Textarea(attrs={'rows': 3, 'class': 'form-control', 'placeholder': 'Descreva a nova interação...'}),
-        }
