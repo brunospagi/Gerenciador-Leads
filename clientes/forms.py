65934fb065795ce37e8aaefc30a7c611ffc87fd5
Bruno Gabriel <brunospagi@gmail.com>
2025-10-15 15:50:01 -0300

    feat: Adiciona campo de data de próximo contato no formulário de cliente

--- clientes/forms.py
+++ clientes/forms.py
@@ -3,6 +3,11 @@ from django import forms
 from .models import Cliente, Historico
 
 class ClienteForm(forms.ModelForm):
+    data_proximo_contato = forms.DateTimeField(
+        required=False,
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'})
+    )
+
     class Meta:
         model = Cliente
         fields = [
@@ -10,7 +15,7 @@ class ClienteForm(forms.ModelForm):
             'marca_veiculo', 'modelo_veiculo', 'ano_veiculo', 'valor_estimado_veiculo',
             'fonte_cliente', 'quantidade_ligacoes', 'tipo_negociacao', 'tipo_contato',
             'status_negociacao', 'proximo_passo', 'prioridade', 'observacao', 'vendedor',
-            'data_proximo_contato' # Adicionado para controle
+            'data_proximo_contato'
         ]
         widgets = {
             'observacao': forms.Textarea(attrs={'rows': 3}),
@@ -19,15 +24,16 @@ class ClienteForm(forms.ModelForm):
             'marca_veiculo': forms.HiddenInput(),
             'modelo_veiculo': forms.HiddenInput(),
             'ano_veiculo': forms.HiddenInput(),
-            'data_proximo_contato': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
         }
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
+        # Omitimos o campo de data do loop para não sobrescrever nosso widget customizado
+        for field_name, field in self.fields.items():
+            if field_name != 'data_proximo_contato':
+                 field.widget.attrs.update({'class': 'form-control'})
 
         if user and not user.is_superuser:
             self.fields['vendedor'].disabled = True
