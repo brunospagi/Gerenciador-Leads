7f0303ea5a5f145d36bfb8ea0a6c33915b9709e6
Bruno Gabriel <brunospagi@gmail.com>
2026-03-18 11:26:39 -0300

    fix(folha): import Q in payroll detail view

--- folha_pagamento/views.py
+++ folha_pagamento/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
-from django.db.models import Sum, Count
+from django.db.models import Sum, Count, Q
 from django.utils import timezone
 import calendar
 from datetime import datetime, date
